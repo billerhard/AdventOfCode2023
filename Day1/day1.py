afd74134a3b6df0f9c0b2625d84c6ce1fde193ae
Bill <bill.erhard@gmail.com>
2023-12-24 04:15:44 -0800

    formatting

--- Day1/day1.py
+++ Day1/day1.py
@@ -5,14 +5,14 @@ import re
 
 with open("Day1\\input", "r", encoding="utf-8") as inputfile:
     SUMVALUES = 0
-    WORDS=["one","two","three","four","five","six","seven","eight","nine"]
-    REGEX=r"one|two|three|four|five|six|seven|eight|nine|\d"
+    WORDS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    REGEX = r"one|two|three|four|five|six|seven|eight|nine|\d"
     for line in inputfile:
-        first=re.search(rf"{REGEX}",line)[0]
-        last=re.findall(rf"(?=({REGEX}))",line)[-1]
+        first = re.search(rf"{REGEX}", line)[0]
+        last = re.findall(rf"(?=({REGEX}))", line)[-1]
         if first in WORDS:
-            first=WORDS.index(first)+1
+            first = WORDS.index(first) + 1
         if last in WORDS:
-            last=WORDS.index(last)+1
-        SUMVALUES+=int(str(first)+str(last))
+            last = WORDS.index(last) + 1
+        SUMVALUES += int(str(first) + str(last))
     print(f"{SUMVALUES}")
