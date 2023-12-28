0bbe7aee12c1f0d36210ec7e5b5c4f4de401c66b
Bill <bill.erhard@gmail.com>
2023-12-28 01:29:35 -0800

    part 1

--- Day2/day2.py
+++ Day2/day2.py
@@ -3,5 +3,29 @@ Figure out possible games and add them up
 """
 
 with open("Day2\\input", "r", encoding="utf-8") as inputfile:
+    sumgames = 0
+    cubetypes = {"red": 12, "green": 13, "blue": 14}
+
     for line in inputfile:
-        print(line)
+        validgame=True
+        gameno, hands = line.split(":")
+        gameno = gameno.strip()
+        hands = hands.strip().split(";")
+        print(gameno)
+        for hand in hands:
+            if not validgame:
+                break
+            cubes = hand.strip().split(",")
+            # print("new hand")
+            for cube in cubes:
+                if not validgame:
+                    break
+                number, cubetype = cube.strip().split(" ")
+                # print("new cube values")
+                # print(number)
+                # print(cubetype)
+                validgame = int(number) <= cubetypes[cubetype]
+                print(f"Is {number} a valid number? It should be less than {cubetypes[cubetype]}.")
+                # print(f"{validgame}")
+        sumgames+= int(gameno.split(" ")[1].strip()) if validgame else 0
+        print(sumgames)
