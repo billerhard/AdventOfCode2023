793c8525d9681f4044bbafe5b758b33d8808d07a
Bill <bill.erhard@gmail.com>
2023-12-28 21:01:18 -0800

    540037

--- Day3/day3.py
+++ Day3/day3.py
@@ -2,62 +2,37 @@
 add up some part numbers
 """
 import re
-arr = [[0 for i in range(141)] for j in range(140)]
-def check_pos(start, end):
-    """
-    find symbols near pos
-    """
-    for x in range(max(start[0] - 1,0), min(end[0] + 2, 139)):
-        for y in range(max(start[1] - 1,0), min(end[1] + 2, 139)):
-            if re.search(r"[^\.\d\n]", str(arr[x][y])):
-                print(f"found symbol: {arr[x][y]} near number: {get_num(start, end)}")
+
+def check_for_symbols(lines, line_number, span):
+    '''
+    check for symbols near line_number,span in lines
+    '''
+    line_range=range(max(line_number-1,0),min(line_number+2,140))
+    span_range=range(max(span[0]-1,0),min(span[1]+1,140))
+    for i in line_range:
+        for j in span_range:
+            if re.search(r"[^\d\.\n]",lines[i][j]):
+                # print(f"found symbol {lines[i][j]} at position {i,j}")
                 return True
     return False
-def get_num(start, end):
-    """
-    turn numbers into strings of numbers
-    """
-    strnum = ""
-    for x in range(start[0], end[0] + 1):
-        for y in range(start[1], end[1]):
-            strnum = strnum + arr[x][y]
-    return strnum
+
 def main():
     '''
     main
     '''
+    sumnum=0
     with open("Day3\\input", "r", encoding="utf-8") as inputfile:
-        startpos = endpos = x, y = 0, 0
-        reading_number = False
-        sum_num = 0
-        for line in inputfile:
-            for character in line:
-                arr[x][y] = character
-                y += 1
-            x += 1
-            y = 0
-        x,y=0,0
-        print(x,y)
-    with open("Day3\\input", "r", encoding="utf-8") as inputfile:
-        for line in inputfile:
-            # print(line)
-            for character in line:
-                match = re.search(r"\d", arr[x][y])
-                if reading_number:
-                    if match:
-                        pass
-                    else:
-                        endpos = x, y
-                        reading_number = False
-                        add_num = check_pos(startpos, endpos)
-                        if add_num:
-                            sum_num += int(get_num(startpos, endpos))
-                            print(sum_num)
-                else:
-                    if match:
-                        startpos = x, y
-                        reading_number = True
-                y += 1
-            x += 1
-            y = 0
+        lines=inputfile.readlines()
+    i=0
+    for line in lines:
+        matches=re.findall(r"\d+", line)
+        for number in matches:
+            match=re.search(rf"{number}", line)
+            found_symbol=check_for_symbols(lines,i,match.span())
+            if found_symbol:
+                print(f"adding number: {number} to sum due to found symbol.")
+                sumnum+=int(number)
+
+        i+=1
+    print(sumnum)
 main()
