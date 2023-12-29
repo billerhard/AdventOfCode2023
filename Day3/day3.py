7d294998abe115764d24e2778888b7f04ffc9e4b
Bill <bill.erhard@gmail.com>
2023-12-28 19:58:51 -0800

    still off

--- Day3/day3.py
+++ Day3/day3.py
@@ -2,25 +2,17 @@
 add up some part numbers
 """
 import re
-
 arr = [[0 for i in range(141)] for j in range(140)]
-
-
 def check_pos(start, end):
     """
     find symbols near pos
     """
     for x in range(max(start[0] - 1,0), min(end[0] + 2, 139)):
         for y in range(max(start[1] - 1,0), min(end[1] + 2, 139)):
-            print(x, y, arr[x][y])
-            character=arr[x][y]
             if re.search(r"[^\.\d\n]", str(arr[x][y])):
-                print(f"found symbol near number: {arr[x][y]}")
+                print(f"found symbol: {arr[x][y]} near number: {get_num(start, end)}")
                 return True
-    # print("did not find symbol")
     return False
-
-
 def get_num(start, end):
     """
     turn numbers into strings of numbers
@@ -29,41 +21,43 @@ def get_num(start, end):
     for x in range(start[0], end[0] + 1):
         for y in range(start[1], end[1]):
             strnum = strnum + arr[x][y]
-
     return strnum
-
-
-with open("Day3\\input", "r", encoding="utf-8") as inputfile:
-    startpos = endpos = x, y = 0, 0
-    readingnumber = False
-    sumnum = 0
-    for line in inputfile:
-        for character in line:
-            arr[x][y] = character
-            y += 1
-        x += 1
-        y = 0
-    x,y=0,0
-    for line in arr[x]:
-        for character in line[y]:
-            match = re.search(r"\d", character)
-            if readingnumber:
-                if match:
-                    pass
+def main():
+    '''
+    main
+    '''
+    with open("Day3\\input", "r", encoding="utf-8") as inputfile:
+        startpos = endpos = x, y = 0, 0
+        reading_number = False
+        sum_num = 0
+        for line in inputfile:
+            for character in line:
+                arr[x][y] = character
+                y += 1
+            x += 1
+            y = 0
+        x,y=0,0
+        print(x,y)
+    with open("Day3\\input", "r", encoding="utf-8") as inputfile:
+        for line in inputfile:
+            # print(line)
+            for character in line:
+                match = re.search(r"\d", arr[x][y])
+                if reading_number:
+                    if match:
+                        pass
+                    else:
+                        endpos = x, y
+                        reading_number = False
+                        add_num = check_pos(startpos, endpos)
+                        if add_num:
+                            sum_num += int(get_num(startpos, endpos))
+                            print(sum_num)
                 else:
-                    endpos = x, y
-                    readingnumber = False
-                    # print(f"number: {get_num(startpos, endpos)}")
-                    addnum = check_pos(startpos, endpos)
-
-                    if addnum:
-                        sumnum += int(get_num(startpos, endpos))
-
-            else:
-                if match:
-                    startpos = x, y
-                    readingnumber = True
-            y += 1
-        x += 1
-        y = 0
-print(sumnum)
+                    if match:
+                        startpos = x, y
+                        reading_number = True
+                y += 1
+            x += 1
+            y = 0
+main()
